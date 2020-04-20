e7714c44b7e8ded2c82d3613860b868e2874c69a
Sergio Ramiro <sergio.ramiro87@gmail.com>
2020-04-20 13:18:55 +0200

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => (
             <span className="text-primary">{config.lastName}</span>
           </h1>
           <div className="subheading mb-5">
-            {config.address} · {config.phone} ·
+            {/* {config.address} · {config.phone} · */}
             <a href={`mailto:${config.email}`}>{config.email}</a>
           </div>
           <p className="lead mb-5">
