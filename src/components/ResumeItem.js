4a1adc70f65f6fb48a8e298ca5e6708b5de10b0e
Sergio Ramiro <sergio.ramiro87@gmail.com>
2020-04-21 10:40:30 +0200

    Created component resumeItem and changed in index

---
+++ src/components/ResumeItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class ResumeItem extends Component {
+    render() {
+        return (<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="resume-content">
+                <h3 className="mb-0">{this.props.position}</h3>
+                <div className="subheading mb-3">{this.props.company}</div>
+                <p>{this.props.text}</p>
+            </div>
+            <div className="resume-date text-md-right">
+                <span className="text-primary">{this.props.data}</span>
+            </div>
+        </div>)
+    }
+}
